0972cc2f63218995d73be3e22dc2d984d32af806
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2022-08-12 10:22:36 -0300

    feat: normalize urls defined in route config

--- modules/Routing/scripts/generateWebRoutes.ts
+++ modules/Routing/scripts/generateWebRoutes.ts
@@ -1,8 +1,13 @@
 import fs from "fs";
 import { RoutesDefinition } from "../config/routes.web";
 
+const normalizeUrl = (url: string) => {
+  const firstChar = url[0];
+  return firstChar === "/" ? url.slice(1) : url;
+};
+
 const routes = RoutesDefinition.map((route) => {
-  const filename = route.index ? "index" : route.url;
+  const filename = route.index ? "index" : normalizeUrl(route.url);
 
   return {
     filename: `pages/${filename}.tsx`,
