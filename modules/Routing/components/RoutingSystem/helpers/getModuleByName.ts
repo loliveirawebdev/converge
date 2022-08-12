d4aa5441f3692b014380ac8f1687703af6fc6701
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2022-08-12 18:32:50 -0300

    feat: throw error when module name is invalid

--- modules/Routing/components/RoutingSystem/helpers/getModuleByName.ts
+++ modules/Routing/components/RoutingSystem/helpers/getModuleByName.ts
@@ -2,5 +2,10 @@ import { ModuleInstances } from "@/modules/instances";
 
 export function getModuleByName(name: string) {
   const definition = ModuleInstances.find((module) => module.name === name);
+
+  if (!definition) {
+    throw new Error(`[getModuleName] instance not found (${name})`);
+  }
+
   return definition.module;
 }
