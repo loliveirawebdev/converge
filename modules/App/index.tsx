bad52b85644f48ce0d0f1efe58d80f5ca10a9636
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2022-08-16 17:20:46 -0300

    feat: use app module in nextjs

--- modules/App/index.tsx
+++ modules/App/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import GlobalState from "@/modules/GlobalState/contexts/GlobalState";
 import RoutingSystem from "@/modules/Routing/components/RoutingSystem";
 
-export default function App() {
+export default function App(props: any) {
   return (
     <GlobalState>
       <RoutingSystem />
+        {props.children}
     </GlobalState>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "setimmediate";
 import React from "react";
+import App from "@/modules/App";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <App>
+      <Component {...pageProps} />
+    </App>
+  );
 }
 
 export default MyApp;
