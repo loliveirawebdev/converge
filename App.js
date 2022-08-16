73882668d3f0f84b2b80f6b55faeab1ca654472c
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2022-08-16 18:45:49 -0300

    chore: add intl polyfill for android

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+// intl polyfill
+import "intl";
+import "intl/locale-data/jsonp/en";
+import "intl/locale-data/jsonp/pt-BR";
+
 // Re-export the root component from the Next.js website
 // as the root component for the native React app.
 export { default } from "@/modules/App";
