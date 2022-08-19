5664300b37f60abcc2ef22e46826e13557d06a2a
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2022-08-19 20:13:47 -0300

    chore: possibilitate quick state test in home

--- modules/Home/index.tsx
+++ modules/Home/index.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import { View, Text } from "react-native";
 import { useNavigation } from "@/modules/Routing";
 import { useTranslation } from "@/modules/Localization";
+import { useGlobalState } from "../GlobalState";
+import { observer } from "mobx-react";
 
-export default function Home() {
+const Home = () => {
   const { t } = useTranslation();
+  const { localizationStore } = useGlobalState();
   const navigation = useNavigation();
   const onPress = () => navigation.navigate({ to: "Category" });
+  const changeLocale = (locale) => () => localizationStore.changeLocale(locale);
 
   return (
     <View>
       <Text onPress={onPress}>{t("welcome")}</Text>
+      <Text onPress={changeLocale("en")}>Change Locale to EN</Text>
+      <Text onPress={changeLocale("pt-BR")}>Change Locale to BR</Text>
     </View>
   );
-}
+};
+
+export default observer(Home);
