da034e0ec4758302adfaf450428b03c6aa2928dc
Lucas Oliveira <loliveirawebdev@gmail.com>
2022-08-22 18:59:46 -0300

    chore: force page crash in home component

--- modules/Home/index.tsx
+++ modules/Home/index.tsx
@@ -6,15 +6,22 @@ import { useGlobalState } from "../GlobalState";
 import { observer } from "mobx-react";
 
 const Home = () => {
+  const [error, setError] = React.useState(false);
+
+  if (error) {
+    throw new Error();
+  }
+
   const { t } = useTranslation();
   const { localizationStore } = useGlobalState();
   const navigation = useNavigation();
-  const onPress = () => navigation.navigate({ to: "Category" });
+  const onPress = () => navigation.navigate({ to: "XXXX" });
   const changeLocale = (locale) => () => localizationStore.changeLocale(locale);
 
   return (
     <View>
       <Text onPress={onPress}>{t("welcome")}</Text>
+      <Text onPress={() => setError(true)}>Break</Text>
       <Text onPress={changeLocale("en")}>Change Locale to EN</Text>
       <Text onPress={changeLocale("pt-BR")}>Change Locale to BR</Text>
     </View>
